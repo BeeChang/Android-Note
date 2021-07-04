e6cfd585430ac32a7763692955c13c5ca906bef4
beechang <dbsckd92@naver.com>
2021-07-04 19:42:30 +0900

    ADD 프로젝트 셋팅 - 데이터바인딩추가 , 테마화이트로만 가능하도록변경 , 색상추가

--- app/src/main/java/com/example/androidnote/MainActivity.kt
+++ app/src/main/java/com/example/androidnote/MainActivity.kt
@@ -2,12 +2,26 @@ package com.example.androidnote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.androidnote.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this , R.layout.activity_main)
+
+        clickEvent();
+
     }
 
+    fun clickEvent(){
+        binding.moveNoteBtn.setOnClickListener{
+//            주제에 맞는 프로젝트로 이동
+
+        }
+    }
 }
